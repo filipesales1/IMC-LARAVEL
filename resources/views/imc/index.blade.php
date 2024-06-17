61fffeb0e33c4e7278e43f770e1b2a8625fc4542
filipe sales <luizfilipessales@gmail.com>
2024-06-17 13:52:15 -0300

    primeira-Laravel

---
+++ resources/views/imc/index.blade.php
@@ -0,0 +1,9 @@
+<x-layout>
+
+
+    <h1>IMC</h1>
+
+    PESO
+    ALTURA
+
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\imcController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/imc',[imcController::class, 'index']);
